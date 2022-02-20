389304679cdd86ac394721888f002d6e9eb19aef
brauMontalvo <79165514+brauMontalvo@users.noreply.github.com>
2022-02-19 21:17:42 -0600

    Add files via upload

---
+++ src/asweb/ASWEB.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package asweb;
+
+import asweb.view.isc.FrmPrincipal;
+
+/**
+ *
+ * @author jol
+ */
+public class ASWEB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrmPrincipal obP = new FrmPrincipal();
+        obP.setSize(640, 480);
+        obP.setVisible(true);
+        obP.setResizable(true);
+        obP.setLocationRelativeTo(null);
+    }
+}
---
+++ src/asweb/control/isc/CtrlAmoMascota.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package asweb.control.isc;
+
+/**
+ *
+ * @author jol
+ */
+public class CtrlAmoMascota {
+    
+    private String duenio;
+    private String mascota;
+    private String raza;
+    private String vacuna1, vacuna2, vacuna3;
+
+    
+    public CtrlAmoMascota() {
+    }
+
+    
+    public String getDuenio() {
+        return duenio;
+    }
+    public void setDuenio(String duenio) {
+        this.duenio = duenio;
+    }
+
+   
+    public String getMascota() {
+        return mascota;
+    }
+    public void setMascota(String mascota) {
+        this.mascota = mascota;
+    }
+
+    
+    public String getRaza() {
+        return raza;
+    }
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+    
+    
+    public String getVacuna1() {
+        return vacuna1;
+    }
+    public void setVacuna1(String vacuna1) {
+        this.vacuna1 = vacuna1;
+    }
+
+   
+    public String getVacuna2() {
+        return vacuna2;
+    }
+    public void setVacuna2(String vacuna2) {
+        this.vacuna2 = vacuna2;
+    }
+
+
+    public String getVacuna3() {
+        return vacuna3;
+    }
+    public void setVacuna3(String vacuna3) {
+        this.vacuna3 = vacuna3;
+    }
+      
+    
+}
